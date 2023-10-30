18856ab91061403b869a5759fd97b231ba12d454
chandana-45 <singmapallichandana1704@gmail.com>
2023-10-30 11:21:41 +0530

    first commit

---
+++ demopush/src/demopush/Push.java
@@ -0,0 +1,11 @@
+package demopush;
+
+public class Push {
+
+	public static void main(String[] args) {
+		System.out.println("Welocme");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ demopush/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author singa
+ *
+ */
+module demopush {
+}
