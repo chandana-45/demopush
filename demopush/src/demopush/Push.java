ba9b7e2b6de73e2d525d2879ffbc09d1e652fcf0
chandana-45 <singmapallichandana1704@gmail.com>
2023-10-30 12:03:25 +0530

    second commit

--- demopush/src/demopush/Push.java
+++ demopush/src/demopush/Push.java
@@ -4,6 +4,7 @@ public class Push {
 
 	public static void main(String[] args) {
 		System.out.println("Welocme");
+		System.out.println("The cricket leauge is started");
 		// TODO Auto-generated method stub
 
 	}
