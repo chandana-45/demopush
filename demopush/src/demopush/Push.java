d43cff9383bf9e2bc6ac6dde35532aef2997fb08
chandana-45 <143927837+chandana-45@users.noreply.github.com>
2023-10-30 12:07:59 +0530

    Update Push.java

--- demopush/src/demopush/Push.java
+++ demopush/src/demopush/Push.java
@@ -5,6 +5,7 @@ public class Push {
 	public static void main(String[] args) {
 		System.out.println("Welocme");
 		System.out.println("The cricket leauge is started");
+		System.out.println("Are you all excited");
 		// TODO Auto-generated method stub
 
 	}
